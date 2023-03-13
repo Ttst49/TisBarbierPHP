ef4637ac1a9e8f3a15137cfe4fa5bb17d3fc2984
Ttst49 <thibautstachnick@gmail.com>
2023-03-13 16:34:33 +0100

    adding mails

--- core/Controllers/BarberController.php
+++ core/Controllers/BarberController.php
@@ -3,6 +3,11 @@
 namespace Controllers;
 
 use Attributes\UsesEntity;
+/**
+use Swift_Mailer;
+use Swift_Message;
+use Swift_SmtpTransport;
+**/
 
 #[UsesEntity(value: false)]
 class BarberController extends AbstractController
@@ -25,20 +30,54 @@ class BarberController extends AbstractController
         return $this->render("barber/prestations",["pageTitle"=>"Préstations","css"=>"prestations"]);
     }
 
+
+
     public function contact(){
 
         $nom = null;
         $mail = null;
+        $message = null;
 
 
-        if (!empty($_POST["nom"])&& !empty($_POST["mail"])){
-            $nom = $_POST["nom"];
-            $mail = $_POST["mail"];
+        if (!empty($_POST["nom"]) && !empty($_POST["mail"])){
+
+            return $this->render("barber/contact",["pageTitle"=>"Contact","css"=>"contact","nom"=>$nom,"mail"=>$mail]);
         }
 
 
-        return $this->render("barber/contact",["pageTitle"=>"Contact","css"=>"contact","nom"=>$nom,"mail"=>$mail]);
+        if (!empty($_POST["nom"])){
+            $nom = $_POST["nom"];
+
+        }
+
+        if (!empty($_POST["mail"])){
+            $mail = $_POST["mail"];
+        }
+
+        if (!empty($_POST["message"])){
+            $message = $_POST["message"];
+        }
+
+        if (!empty($_POST["nom"]) && (!empty($_POST["mail"])) && (!empty($_POST["message"]))){
+
+            $transport = (new Swift_SmtpTransport("smtp.example.org",25))
+                ->setUsername("heu")
+                ->setPassword("jsp");
+
+            $mailer = new Swift_Mailer($transport);
+
+            $message = (new Swift_Message("Prise contact TisBarbier"))
+                ->setFrom([$mail=>$nom])
+                ->setTo(["thibautstachnick@gmail.com"=>"thithi stach"])
+                ->setBody($message);
+
+            $result = $mailer->send($message);
+
+        }
+
+
     }
+
 
     public function mentions(){
         return $this->render("barber/mentionsLégales",["pageTitle"=>"Mention Légales"]);
@@ -48,8 +87,4 @@ class BarberController extends AbstractController
         return $this->render("barber/connect",["pageTitle"=>"Connexion Admin"]);
     }
 
-    /**
-     *
-     * création d'une méthode mail?
-     * */
 }
