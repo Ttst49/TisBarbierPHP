ca6861fe21a296c7a504f68a0d0eb6e733d809b7
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-03-24 17:07:16 +0100

    add animations

--- core/Controllers/BarberController.php
+++ core/Controllers/BarberController.php
@@ -54,20 +54,22 @@ class BarberController extends AbstractController
 
 
 
-            $url = 'http://formsubmit.co/thibautstachnick@gmail.com';
+            /**$url = 'http://formsubmit.co/thibautstachnick@gmail.com';
             $data = array('nom' => $nom, 'mail' => $mail, "message"=>$message);
 
             $options = array(
                 'http' => array(
-                    'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                    'method'  => 'POST',
+                    'headers'  => "Content-Type: application/x-www-form-urlencoded\r\n",
+                    'method'  => "POST",
                     'content' => http_build_query($data)
                 )
             );
             $context  = stream_context_create($options);
             $result = file_get_contents($url, false, $context);
 
-            var_dump($result);
+            var_dump($result);**/
+
+
 
 
         }
