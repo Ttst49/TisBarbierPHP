230a688c0de4cc945a45e0c0950913f8c88dcf34
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-04-11 10:16:58 +0200

    change File.php in order to add a second directory

--- core/App/File.php
+++ core/App/File.php
@@ -12,18 +12,23 @@ class File
 
 
     private string $uploadDirectory;
+    private string $secondUploadDirectory;
     private mixed $fileData;
     private string|array $extension;
     private mixed $tempFile;
     private string $name;
     private string $target;
+    private string $secondTarget;
     private mixed $mimeType;
     private array $acceptedMimeTypes = ["image/jpg","image/jpeg", "image/png", "image/gif"];
 
     public function __construct($index)
     {
         //Manage the directory where files are taken
-        $this->uploadDirectory = dirname(__DIR__, 2) . "/image/";
+        $this->uploadDirectory = dirname(__DIR__, 2) . "public/image/";
+
+        //Manage the admin directory where files are taken
+        $this->secondUploadDirectory = dirname(__DIR__, 2) . "admin/image/";
 
         //Manage the file itself and get it information
         $this->fileData = $_FILES[$index];
@@ -36,6 +41,9 @@ class File
 
         //Manage the target of the file after we used it
         $this->target = $this->uploadDirectory.$this->name;
+
+        //Manage the second target of the file after we used it
+        $this->secondTarget = $this->secondUploadDirectory.$this->name;
 
         //Params
         $this->mimeType = $this->fileData["type"];
@@ -55,6 +63,7 @@ class File
             }
 
             move_uploaded_file($this->tempFile, $this->target);
+            move_uploaded_file($this->tempFile, $this->secondTarget);
 
     }
 
