72d0d678fd9f719fca46693e9cb77d971a48b0eb
Thibaut Stachnick <thibautstachnick@gmail.com>
2023-04-12 17:08:15 +0200

    change File.php in order to make it work by default on a server

--- core/App/File.php
+++ core/App/File.php
@@ -24,7 +24,7 @@ class File
     public function __construct($index)
     {
         //Manage the directory where files are taken
-        $this->uploadDirectory = dirname(__DIR__, 2) . "public/image/";
+        $this->uploadDirectory = dirname(__DIR__, 3) . "/tisBarbier/public/image/";
 
         //Manage the file itself and get it information
         $this->fileData = $_FILES[$index];
